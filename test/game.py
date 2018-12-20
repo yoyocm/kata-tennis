a9b27db2103609e64207e06cc2cf3c1cd8029ceb
Boj Yoann <yoann.boj@safrangroup.com>
2018-12-20 23:38:06 +0100

    test

--- test/game.py
+++ test/game.py
@@ -1,7 +1,12 @@
 import unittest
 
-
 class TestPackages(unittest.TestCase):
+    def setUp(self):
+        """Before unittest"""
+        pwd = os.path.dirname(__file__)
+        filename = "%s/%s" % (pwd, "packages.cache")
+        self.__allpackages = loadPkgInfo(filename, False, True)
+
     def test_summary(self):
-        """First dummy test"""
+        """ test summary properties"""
         self.assertEqual(True, True)
